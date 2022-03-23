7c2925bb68f1ca4bed2d0b210b87f371bd451a39
LoopFruits <Kensworldc@gmail.com>
2022-03-23 14:45:34 -0400

    commit tree fa67439e79897675466ffeee52f4b1100ed895bd

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,16 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game 
+  def game 
+    game.find(self.game_id)
+  end
 end
+
+# rake console : 
+
+# # Access the first review instance in the database
+# review = Review.first
+# # => #<Review:0x00007ffc23c58e20 id: 1, score: 6, comment: "Velit a tenetur eius.", game_id: 1>
+
+# # Get the game_id foreign key for the review instance
+# review.game_id
+# # => 1
